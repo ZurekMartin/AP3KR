784d9f1fac64fd08567ba5833a11ce318168fe2a
ZurekMartin <146104663+ZurekMartin@users.noreply.github.com>
2023-10-20 16:53:07 +0200

    Update main.py

--- AfinniSifra/main.py
+++ AfinniSifra/main.py
@@ -12,8 +12,8 @@ root.title("Affine Cipher")
 # Defining alphabet and variable
 lower_case = list(map(chr, range(ord('a'), ord('z') + 1)))
 upper_case = list(map(chr, range(ord('A'), ord('Z') + 1)))
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-alphabet = lower_case + upper_case + numbers
+digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+alphabet = lower_case + upper_case + digits
 alphabet_length = len(alphabet)
 
 
